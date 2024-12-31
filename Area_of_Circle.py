42912e7063b584c687a93ec36f8bbe0c7b865635
mntejesh <tmakena@student.gitam.edu>
2024-12-31 15:09:35 +0530

    Create Area_of_Circle.py

---
+++ Area_of_Circle.py
@@ -0,0 +1,13 @@
+radius = int(input())
+
+AREA = 3.14*(radius**2)
+
+print("Area of the circle:",AREA)
+
+print("Area of the circle:",AREA,sep="")
+
+print(f"Area of the circle: {AREA}")
+
+
+print("Area of the circle:",AREA)
+
