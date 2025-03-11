0c3b8f0f200e258e84bd6d5a7ddb4b755d190688
mntejesh <tmakena@student.gitam.edu>
2025-03-11 15:26:54 +0530

    Create 10_natural.c

---
+++ 10_natural.c
@@ -0,0 +1,14 @@
+
+15. Write a C program to calculate sum of 10 natural numbers
+CODE : #include <stdio.h>
+           int main()
+      {
+          int i, sum = 0;
+          // Calculate sum of 10 natural numbers
+          for (i = 1; i <= 10; i++)
+          {
+              sum += i;
+           }
+              printf("Sum of 10 natural numbers: %d\n", sum);
+              return 0;
+        }
